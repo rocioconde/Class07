7cab48642898c33f30a8554890305fd246e2325c
rocionde <rec2146@tc.columbia.edu>
2017-02-27 19:31:07 -0500

    New project

---
+++ app/src/main/java/com/example/android/class07/Class07Activity.java
@@ -0,0 +1,13 @@
+package com.example.android.class07;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Class07Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class07);
+    }
+}

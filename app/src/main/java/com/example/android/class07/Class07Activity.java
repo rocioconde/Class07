b6b87c06a09ba1ad33ecf62daf37e48fd6d1e68f
rocionde <rec2146@tc.columbia.edu>
2017-02-27 19:40:25 -0500

    Added display text view and a person class

--- app/src/main/java/com/example/android/class07/Class07Activity.java
+++ app/src/main/java/com/example/android/class07/Class07Activity.java
@@ -2,12 +2,17 @@ package com.example.android.class07;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Class07Activity extends AppCompatActivity {
+
+    private TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class07);
+
+        displayText = (TextView) findViewById(R.id.display_text);
     }
 }
---
+++ app/src/main/java/com/example/android/class07/Person.java
@@ -0,0 +1,8 @@
+package com.example.android.class07;
+
+/**
+ * Created by ccteuser on 2/27/17.
+ */
+
+public class Person {
+}
